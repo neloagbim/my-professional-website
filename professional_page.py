34dc55966a9cc0e94cc7c70474ae3ee263c01477
Nelo Agbim <47784696+neloagbim@users.noreply.github.com>
2024-01-10 18:05:30 -0700

    Update professional_page.py
    added columns

--- professional_page.py
+++ professional_page.py
@@ -8,7 +8,8 @@ Created on Wed Jan 10 15:15:48 2024
 import pandas as pd
 import streamlit as st
 
-
-st.title("Chinelo Agbim's Porfolio")
-st.header("About me")
-st.text("I'm an analyst with a passion for helping people through data. I am particularly interested in Clean Energy and creating a more equitable world.")
+col1,col2 = st.columns([1,4])
+with col2:
+  st.title("Chinelo Agbim's Porfolio")
+  st.header("About me")
+  st.text("I'm an analyst with a passion for helping people through data. I am particularly interested in Clean Energy and creating a more equitable world.")
