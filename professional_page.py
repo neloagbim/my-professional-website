3e7249078cd18a291892861b14aab5d91e5029bd
Nelo Agbim <cnagbim@gmail.com>
2024-01-10 17:39:14 -0700

    Create professional_page.py

---
+++ professional_page.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Jan 10 15:15:48 2024
+
+@author: NeloAgbim
+"""
+
+import pandas as pd
+import streamlit as st
+import pyodbc
+
+
+st.title("Chinelo Agbim's Porfolio")
+st.header("About me")
+st.text("I'm an analyst with a passion for helping people through data. n/ I am particularly interested in Clean Energy and creating a more equitable world.")
